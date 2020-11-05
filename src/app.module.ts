c0593b3464c6ada9fd5f13df94b62df8fcbb8c0c
shortnd <ococncol@gmail.com>
2020-11-05 09:42:10 -0500

    feat: added typeorm and base setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChannelsModule } from './channels/channels.module';
 
 @Module({
-  imports: [ChannelsModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'toor',
+    database: 'postgres',
+    autoLoadEntities: true,
+    synchronize: true
+  }),
+  ChannelsModule
+],
   controllers: [AppController],
   providers: [AppService],
 })
