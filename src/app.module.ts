e0f06745ebd2bdfc541fd8aa683a258709abed3f
shortnd <ococncol@gmail.com>
2020-11-05 09:35:41 -0500

    feat: starting channels

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ChannelsModule } from './channels/channels.module';
 
 @Module({
-  imports: [],
+  imports: [ChannelsModule],
   controllers: [AppController],
   providers: [AppService],
 })
